8a5f52edc10b92118bc4d609118e65504643977f
DengJunYuan <540997806@qq.com>
2019-03-12 08:46:51 +0800

    使用核心模式正确地绘制出布料

---
+++ Core-Profile/Cloth.h
@@ -0,0 +1,151 @@
+#pragma once
+#include <ClothSimulation\Particles.h>
+#include <math.h>
+#include <vector>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+class Cloth
+{
+private:
+	int num_particles_width; // number of particles in "width" direction
+	int num_particles_height; // number of particles in "height" direction
+	// total number of particles is num_particles_width*num_particles_height
+
+	std::vector<Particle> particles; // all particles that are part of this cloth
+
+	Particle* getParticle(int x, int y) { return &particles[y*num_particles_width + x]; }
+	//在内存中一份顶点数据
+	std::vector<float>vertices;
+	std::vector<unsigned int>indices;
+	//绘制相关
+	unsigned int VBO, VAO, EBO;
+
+public:
+
+	/* This is a important constructor for the entire system of particles and constraints*/
+	Cloth(float width, float height, int num_particles_width, int num_particles_height) : num_particles_width(num_particles_width), num_particles_height(num_particles_height)
+	{
+		particles.resize(num_particles_width*num_particles_height); //I am essentially using this vector as an array with room for num_particles_width*num_particles_height particles
+
+		// creating particles in a grid of particles from (0,0,0) to (width,-height,0)
+		for (int x = 0; x < num_particles_width; x++)
+		{
+			for (int y = 0; y < num_particles_height; y++)
+			{
+				vec3 pos = vec3(width * (x / (float)num_particles_width),
+					-height * (y / (float)num_particles_height),
+					0);
+				particles[y*num_particles_width + x] = Particle(pos); // insert particle in column x at y'th row
+				//同样保存一份在内存中：
+				vertices.push_back(pos.x);
+				vertices.push_back(pos.y);
+				vertices.push_back(pos.z);
+			}
+		}
+		//填充索引数据到indices中
+		for (int x = 0; x < num_particles_width - 1; x++)
+		{
+			for (int y = 0; y < num_particles_height - 1; y++)
+			{
+				indices.push_back((x)*num_particles_height + y + 1);
+				indices.push_back((x + 1)*num_particles_height + y + 1);
+				indices.push_back((x)*num_particles_height + y);
+
+				indices.push_back((x + 1)*num_particles_height + y+1);
+				indices.push_back((x+1)*num_particles_height + y);
+				indices.push_back((x)*num_particles_height + y);
+			}
+		}
+
+		// making the upper left most three and right most three particles unmovable
+		for (int i = 0; i < 3; i++)
+		{
+			getParticle(0 + i, 0)->offsetPos(vec3(0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+			getParticle(0 + i, 0)->makeUnmovable();
+
+			getParticle(0 + i, 0)->offsetPos(vec3(-0.5, 0.0, 0.0)); // moving the particle a bit towards the center, to make it hang more natural - because I like it ;)
+			getParticle(num_particles_width - 1 - i, 0)->makeUnmovable();
+		}
+		//初始化gl缓存对象
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float)*vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*indices.size(), &indices[0], GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindVertexArray(0);
+	}
+	~Cloth() {
+		// optional: de-allocate all resources once they've outlived their purpose:
+	    // ------------------------------------------------------------------------
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &EBO);
+	}
+	vec3 calcTriangleNormal(Particle *p1, Particle *p2, Particle *p3)
+	{
+		vec3 pos1 = p1->getPos();
+		vec3 pos2 = p2->getPos();
+		vec3 pos3 = p3->getPos();
+
+		vec3 v1 = pos2 - pos1;
+		vec3 v2 = pos3 - pos1;
+
+		return cross(v1, v2);
+		
+	}
+	/* drawing the cloth as a smooth shaded (and colored according to column) OpenGL triangular mesh
+	Called from the display() method
+	The cloth is seen as consisting of triangles for four particles in the grid as follows:
+
+	(x,y)   *--* (x+1,y)
+			| /|
+			|/ |
+	(x,y+1) *--* (x+1,y+1)
+
+	*/
+	void drawShaded()
+	{
+		// reset normals (which where written to last frame)
+		std::vector<Particle>::iterator particle;
+		for (particle = particles.begin(); particle != particles.end(); particle++)
+		{
+			(*particle).resetNormal();
+		}
+
+		//create smooth per particle normals by adding up all the (hard) triangle normals that each particle is part of
+		for (int x = 0; x < num_particles_width - 1; x++)
+		{
+			for (int y = 0; y < num_particles_height - 1; y++)
+			{
+				vec3 normal = calcTriangleNormal(getParticle(x + 1, y), getParticle(x, y), getParticle(x, y + 1));
+				getParticle(x + 1, y)->addToNormal(normal);
+				getParticle(x, y)->addToNormal(normal);
+				getParticle(x, y + 1)->addToNormal(normal);
+
+				normal = calcTriangleNormal(getParticle(x + 1, y + 1), getParticle(x + 1, y), getParticle(x, y + 1));
+				getParticle(x + 1, y + 1)->addToNormal(normal);
+				getParticle(x + 1, y)->addToNormal(normal);
+				getParticle(x, y + 1)->addToNormal(normal);
+			}
+		}
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6* (num_particles_height-1)*(num_particles_width-1), GL_UNSIGNED_INT, 0);
+	}
+};
---
+++ include/ClothSimulation/Particles.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp> //for matrices
+#include <glm/gtc/type_ptr.hpp>
+const float DEFAULT_DAMPING = -0.0125f;
+using namespace glm;
+
+class Particle
+{
+private:
+	vec3 pos;
+	vec3 old_pos;
+	vec3 accumulated_normal; // an accumulated normal (i.e. non normalized), used for OpenGL soft shading
+	vec3 acceleration;
+	vec3 velocity;
+	float mass;
+	bool movable;
+public:
+	Particle(vec3 pos) : pos(pos), old_pos(pos), acceleration(vec3(0, 0, 0)), mass(1), movable(true), accumulated_normal(vec3(0, 0, 0)) {}
+	Particle() {}
+	vec3& getPos() { return pos; }
+	vec3& getLastPos() { return old_pos; }
+	void resetNormal() { accumulated_normal = vec3(0, 0, 0); }
+	vec3& getNormal() { return accumulated_normal; } // notice, the normal is not unit length
+	vec3& getVelocity() {
+		return velocity;
+	}
+	void addToNormal(vec3 normal)
+	{
+		accumulated_normal = accumulated_normal + normalize(normal);
+	}
+
+	void addForce(vec3 f)
+	{
+		acceleration += f / mass;
+	}
+
+	void timeStep(float dt)
+	{
+		if (movable)
+		{
+			vec3 temp = pos;
+			pos = pos + (pos - old_pos) + acceleration * dt*dt;
+			old_pos = temp;
+			acceleration = vec3(0, 0, 0); // acceleration is reset since it HAS been translated into a change in position (and implicitely into velocity)
+
+			if (pos.y <0) {
+				pos.y = 0;
+			}
+		}
+	}
+	void makeUnmovable() { movable = false; }
+	void offsetPos(const vec3 v) 
+	{ 
+		if (movable) pos += v;
+	}
+	void setPos(const vec3 p)
+	{
+		if (movable) pos = p;
+	}
+};
