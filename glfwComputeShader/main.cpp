76b66404b328d35032e6a1781ff8bf37f9b4156b
DengJunYuan <540997806@qq.com>
2019-04-28 10:38:51 +0800

    正确应用简单下落的computeShader

--- glfwComputeShader/main.cpp
+++ glfwComputeShader/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char **argv) {
 	// ------------------------------
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
