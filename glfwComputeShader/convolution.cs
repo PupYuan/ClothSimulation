d69cd0b5f3b73e2468c4332594647325c20c275b
DengJunYuan <540997806@qq.com>
2019-04-29 14:16:21 +0800

    用*/1000的方式，还愿一定位数的浮点数原子操作

--- glfwComputeShader/convolution.cs
+++ glfwComputeShader/convolution.cs
@@ -8,20 +8,22 @@ uniform float v[4];
 layout (rgba32f, binding = 0) uniform image2D input_image;
 layout (rgba32f, binding = 1) uniform image2D output_image;
 
-shared uvec4 scanline[16][16];
+shared ivec4 scanline[16][16];
 
 void main(void)
 {
     ivec2 pos = ivec2(gl_GlobalInvocationID.xy);
-    scanline[pos.x][pos.y] = uvec4(imageLoad(input_image, pos));
+    vec4 temp = vec4(imageLoad(input_image, pos));
+    temp = temp * 1000;
+    scanline[pos.x][pos.y] = ivec4(temp);
     //vec4 data = imageLoad(input_image, pos); 
     barrier();
     ivec2 halfpos = ivec2((pos.x)/2,(pos.y)/2);
-    atomicAdd(scanline[halfpos.x][halfpos.y].x, 1);
-    atomicAdd(scanline[halfpos.x][halfpos.y].y, 1);
-    atomicAdd(scanline[halfpos.x][halfpos.y].z, 1);
-    atomicAdd(scanline[halfpos.x][halfpos.y].w, 1);
-    vec4 data = scanline[pos.x][pos.y];
+    atomicAdd(scanline[halfpos.x][halfpos.y].x, 222);
+    atomicAdd(scanline[halfpos.x][halfpos.y].y, 222);
+    atomicAdd(scanline[halfpos.x][halfpos.y].z, 222);
+    atomicAdd(scanline[halfpos.x][halfpos.y].w, 222);
+    vec4 data = scanline[pos.x][pos.y]/1000.0f;
     //data.r = 0 + data.r;
     //data.g = 1 + data.g;
     //data.b = 2 + data.b;
